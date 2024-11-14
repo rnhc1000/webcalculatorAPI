87244d277c52fcd4e3ab1987452569a8d62ec2a4
Ricardo Ferreira <rnhc1000@hotmail>
2024-11-13 23:17:44 -0300

    [feat] ✨: add security: user must get a token to do maths and must be enabled at the database in adavnce by the administrator

---
+++ src/test/java/br/dev/ferreiras/webcalculatorapi/services/SquareRootServiceTest.java
@@ -0,0 +1,22 @@
+package br.dev.ferreiras.webcalculatorapi.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SquareRootServiceTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void mathOperations() {
+  }
+}
